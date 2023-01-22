cd2edf396843fd0f1953bdeb53f2cf578987cf7e
Chris Yoon <chris7yoon@gmail.com>
2023-01-21 18:36:39 -0800

    added comments, test setup

--- src/graph.py
+++ src/graph.py
@@ -3,51 +3,53 @@ import json
 from collections import deque
 from helpers import Helpers
 from node import Node
+from copy import copy
 
 class Graph:    
     def __init__(self) -> None:
         self.fwd = {} # course -> post-req
         self.back = {} # course -> pre-req
         self.nodes = {} # name -> course
-    """ 
-    Initializes the entire graph structure and 
-    Assuming:
-        - no duplicates in data
-    """    
+
     @staticmethod
     def init_graph():
-        FOLDER = "/home/chris/nwhacks2023/courses/"
-        file_path = Helpers.get_files_from_folder(FOLDER)
+        """Initializes the Graph structure
+
+        Returns:
+            Graph: graph consisting of all courses
+        """
+        file_path = "/home/chris/nwhacks2023/courses.json"
         graph = Graph()
         # get all graph data
-        for f in file_path:
-            with open(f, "r") as file:
-                data = json.load(file)
-                Graph.populate(data, graph)
+        with open(file_path, "r") as file:
+            data = json.load(file)
+            Graph.populate(data, graph)
         return graph
 
     @staticmethod
     def populate(data, graph):
+        """Populates the graph with courses
+
+        Args:
+            data (Any): object representing data from a json file
+            graph (Graph): Graph to populate data onto
+        """
         for d in data:
-            if d["name"] == "PHYS 157":
-                print(d)
             name = d["name"]
             if name in graph.nodes:
                 continue
-            graph.fwd[name] = d["postreqs"] 
-            graph.back[name] = d["prereqs"]
-            graph.back[name].extend(d["coreqs"])
+            graph.fwd[name] = set(d["postreqs"])
+            graph.back[name] = set(d["prereqs"])
+            graph.back[name].update(d["coreqs"])
             graph.nodes[name] = Node(name, d["credits"], d["desc"])
 
-    def bfs(self, src, tgt, pre=False, depth=-1) -> Node:
+    def bfs(self, src, tgt, pre=True, depth=-1):
         que = deque()
         vis = set()
         que.append(src)
         i = 0
-        print(que, vis)
         while que or (depth != -1 and i > depth):
             crs = que.popleft()
-            print(crs)
             if crs in vis:
                 continue
             vis.add(crs)
@@ -59,13 +61,48 @@ class Graph:
                 que.extend(self.fwd[crs])
             i += 1
         return []
+        
+    def findAllPaths(self, src, tgt, pre=False):
+        """Finds all paths between the source and target courses.
+
+        Args:
+            src (str): name of course to start traversal
+            tgt (str): name of course to reach through traversal
+            pre (bool, optional): True if traversal goes backwawrds (i.e. towards pre-reqs). Defaults to False.
+
+        Returns:
+            set[tuple[str, str]]: A set of tuples of two course codes, representing an edge, that is visited throughout traversal.
+        """        
+        valid_paths = set()
+        self.__findAllPathsInner(src, tgt, [], set(), valid_paths, pre)
+        return valid_paths
+
+    def __findAllPathsInner(self, src, tgt, curr_path, curr_path_set, valid_paths, pre=False):
+        # base
+        if src == tgt:
+            valid_paths.update(copy(curr_path))
+            return
+        if src not in self.nodes:
+            courses = set()
+        else:
+            courses = self.back[src] if pre else self.fwd[src]
+        curr_path_set.add(src)
+        for crs in courses:
+            curr_path.append((src,crs))
+            if crs not in curr_path_set:
+                self.findAllPathsInner(crs, tgt, curr_path, curr_path_set, valid_paths, pre)
+                del curr_path[-1]
 
 def main():
     graph = Graph.init_graph()
-    print(graph.fwd["PHYS 157"])
-    res = graph.bfs("PHYS 157", "PHYS 158")
+    # src = "MATH 100"
+    # tgt = "CPSC 221"
+    tgt = "MATH 100"
+    src = "CPSC 221"
+    res = graph.findAllPaths(src, tgt, pre=True)
+    print("\n")
     print(res)
     pass
 
 if __name__ == "__main__":
-    main()
+    main() 
--- src/node.py
+++ src/node.py
@@ -1,5 +1,5 @@
-class Node:
+class Node:    
     def __init__(self, name, cred, desc) -> None:
-        self.id = name # id 
+        self.name = name # id 
         self.cred = cred
         self.desc = desc
