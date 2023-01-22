011ba3289fdb727644fb7ee31ef5c65498a595e9
1mackenziekyle <1mackenziekyle@gmail.com>
2023-01-22 09:20:16 -0800

    Node circle:

--- src/front/Graph.js
+++ src/front/Graph.js
@@ -2,11 +2,31 @@ anychart.onDocumentReady(function () {
   fetch("./anygraph.json")
     .then((response) => response.json())
     .then((data) => {
+      console.log(data);
       var chart = anychart.graph(data);
-      chart.layout().iterationCount(100);
-      // set the title
-      chart.title("Network Graph showing the battles in Game of Thrones");
-      // draw the chart
+      var nodes = chart.nodes();
+      var edges = chart.edges();
+      console.log("Number of Nodes:");
+      chart.title("UBC Course Dependencies Graph");
+
+      /**
+       * APPEARANCE
+       */
+      // EDGES
+      edges.normal().stroke("#F3F3F5", 2);
+      edges.selected().fill("#000000", 2);
+      edges.selected().stroke("#000000", 2);
+
+      // NODES
+      nodes.normal().height(5);
+      nodes.normal().stroke(null);
+
+      nodes.normal().fill("#F0F0F0"); // unselected fill
+      nodes.hovered().stroke("#CFCFCF", 3);
+      nodes.selected().stroke("#CFCFCF", 3);
+      chart.layout().iterationCount(0); // set circle shape
+      chart.interactivity().nodes(false); // disallow moving nodes
+
       chart.container("container").draw();
     });
 });
